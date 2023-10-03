4700317324a2d5a8aaef9054bb825834352063a2
aldomendivil17 <aldomendivil17@gmail.com>
2023-10-03 15:40:18 -0700

    Se modificó clase control de membresia agregando parametros nuevos

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ mongoose.connection.on('open', () => {
 
 const MembresiaController = require('./controllers/membresiaController');
 
+// Datos de prueba
+// const datosNuevaMembresia = {
+//     tipo: '2 semanas',
+//     costo: 700,
+//     fecha_renovacion: new Date(2012, 10, 10)
+//   };
+
 // Ejecutar las operaciones
-MembresiaController.agregarMembresia();
-// MembresiaController.buscarMembresiaPorId('6519e475bd75a9aa07956582');
-// MembresiaController.actualizarMembresiaPorId('6519e475bd75a9aa07956582');
-// MembresiaController.eliminarMembresiaPorId('6519e475bd75a9aa07956582');
+// MembresiaController.agregarMembresia(datosNuevaMembresia);
+// MembresiaController.actualizarMembresiaPorId("6519e6f49c86558810c26754", {costo: 150})
+// MembresiaController.eliminarMembresiaPorId("651c9702c1559867867db62b");
--- controllers/membresiaController.js
+++ controllers/membresiaController.js
@@ -2,31 +2,34 @@ const MembresiaDao = require('../daos/membresiaDao');
 const Membresia = require('../models/membresia');
 
 class MembresiaController {
-  async agregarMembresia() {
-    const nuevaMembresia = new Membresia({
-      tipo: 'Anual',
-      costo: 3500,
-      fecha_renovacion: new Date(2032, 10, 23)
-    });
-
-    await MembresiaDao.agregarMembresia(nuevaMembresia);
-  }
+    async agregarMembresia(datosMembresia) {
+        const nuevaMembresia = new Membresia(datosMembresia);
+    
+        try {
+          await MembresiaDao.agregarMembresia(nuevaMembresia);
+        } catch (error) {
+          console.error('Error al guardar la membresía:', error);
+        }
+      }
 
   async buscarMembresiaPorId(id) {
     await MembresiaDao.buscarMembresiaPorId(id);
   }
 
-  async actualizarMembresiaPorId(id) {
-    const nuevosDatos = {
-      tipo: 'Semanal',
-      costo: 150
-    };
-
-    await MembresiaDao.actualizarMembresiaPorId(id, nuevosDatos);
+  async actualizarMembresiaPorId(id, nuevosDatos) {
+    try {
+        await MembresiaDao.actualizarMembresiaPorId(id, nuevosDatos);
+    } catch (error) {
+        console.error('Error al actualizar la membresía:', error);
+    }   
   }
 
   async eliminarMembresiaPorId(id) {
-    await MembresiaDao.eliminarMembresiaPorId(id);
+    try {
+        await MembresiaDao.eliminarMembresiaPorId(id);
+    } catch (error) {
+        console.error('Error al eliminar la membresía:', error);
+    }
   }
 }
 
