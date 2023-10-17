6a8d5a23b128ad99c0b53748e3bc679f20798d05
aldomendivil17 <aldomendivil17@gmail.com>
2023-10-17 13:55:39 -0700

    Se agregó addMembresias en clase de control

--- models/membresia.js
+++ models/membresia.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const membresiaSchema = new mongoose.Schema({
-    id : Number,
     tipo: String,
     costo: Number,
     fecha_renovacion: Date
 });
 
-module.exports = mongoose.model('Membresia', membresiaSchema);
+module.exports = mongoose.model('Membresia', membresiaSchema); 
--- routes/membresia.js
+++ routes/membresia.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const membresiaControl = require('../controllers/membresiaControl');
 
 // Rutas
-router.get('/', membresiaControl.getAllMembresias);
-// router.post('/membresia', membresiaController.addMembresia);
+router.get('/membresia', membresiaControl.getAllMembresias);
+router.post('/membresia', membresiaControl.addMembresia);
 router.get('/membresia/:id', membresiaControl.getMembresiaById);
 // router.put('/membresia/:id', membresiaController.updateMembresia);
 // router.delete('/membresia/:id', membresiaController.deleteMembresia);
