d73107c266b679885f6546beebdfad9a307f0818
aldomendivil17 <aldomendivil17@gmail.com>
2023-10-18 14:01:56 -0700

    se agregaron las nuevas dao de membresia

--- controllers/membresiaControl.js
+++ controllers/membresiaControl.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-const Membresia = require('../models/membresia');
-const dataPath = './data/membresia.json';
 const MembresiaDAO = require('../daos/membresiaDao');
 
 // Funciones del controlador
@@ -17,7 +15,7 @@ exports.getMembresiaById = async (req, res) => {
   const membresiaId = req.params.id;
 
   try {
-    const membresia = await Membresia.findOne({_id: membresiaId });
+    const membresia = await MembresiaDAO.findMembresiaById(membresiaId);
 
     if (!membresia) {
       return res.status(404).json({ error: 'Membresia no encontrada' });
@@ -37,7 +35,7 @@ exports.addMembresia = async (req, res) => {
       fecha_renovacion: req.body.fecha_renovacion,
     });
 
-    await datosNuevaMembresia.save();
+    await MembresiaDAO.addMembresia(datosNuevaMembresia);
 
     res.json(datosNuevaMembresia);
   } catch (err) {
@@ -54,7 +52,7 @@ exports.updateMembresia = async (req, res) => {
       $set: req.body,
     };
 
-    await Membresia.updateOne(filter, update);
+    await MembresiaDAO.updateMembresia(filter, update);
 
     res.json({ message: 'Membresía actualizada exitosamente' });
 
@@ -67,7 +65,8 @@ exports.deleteMembresia = async (req, res) => {
   const membresiaId = req.params.id;
 
   try {
-    await Membresia.deleteOne({ _id: membresiaId });
+
+    await MembresiaDAO.deleteMembresiaById(membresiaId);
 
     res.json({ message: 'Membresía eliminada exitosamente' });
   } catch (err) {
--- daos/membresiaDao.js
+++ daos/membresiaDao.js
@@ -5,74 +5,21 @@ class MembresiaDao {
     return await Membresia.find({});
   }
 
-
-  async buscarTodasLasMembresias() {
-    try {
-      const membresias = await Membresia.find({});
-      if (membresias && membresias.length > 0) {
-        return membresias;
-      } else {
-        console.log('No se encontraron membresías.');
-        return [];
-      }
-    } catch (error) {
-      console.error('Error al buscar las membresías:', error);
-      throw error;
-    }
+  async findMembresiaById(id) {
+    return await Membresia.findOne({ _id: id });
   }
 
-
-  async agregarMembresia(nuevaMembresia) {
-    try {
-      await nuevaMembresia.save();
-      console.log('Membresía guardada con éxito');
-    } catch (error) {
-      console.error('Error al guardar la membresía:', error);
-    }
+  async addMembresia(membresia) {
+    await membresia.save();
   }
 
-  async buscarMembresiaPorId(id) {
-    try {
-      const membresia = await Membresia.findOne({ id: id });
-      if (membresia) {
-        console.log('Membresía encontrada:', membresia);
-      } else {
-        console.log('Membresía no encontrada');
-      }
-    } catch (error) {
-      console.error('Error al buscar la membresía:', error);
-    }
+  async updateMembresia(filter, update) {
+    await Membresia.updateOne(filter, update);
   }
 
-  async actualizarMembresiaPorId(id, nuevosDatos) {
-    try {
-      const membresiaActualizada = await Membresia.findOneAndUpdate(
-        { id: id },
-        nuevosDatos,
-        { new: true }
-      );
-      if (membresiaActualizada) {
-        console.log('Membresía actualizada con éxito:', membresiaActualizada);
-      } else {
-        console.log('Membresía no encontrada');
-      }
-    } catch (error) {
-      console.error('Error al actualizar la membresía:', error);
-    }
+  async deleteMembresiaById(id) {
+    await Membresia.deleteOne({ _id: id });
   }
-
-  async eliminarMembresiaPorId(id) {
-    try {
-      const membresiaEliminada = await Membresia.findOneAndDelete({ id: id });
-      if (membresiaEliminada) {
-        console.log('Membresía eliminada con éxito:', membresiaEliminada);
-      } else {
-        console.log('Membresía no encontrada');
-      }
-    } catch (error) {
-      console.error('Error al eliminar la membresía:', error);
-    }
-  }
+  
 }
-
 module.exports = new MembresiaDao();
