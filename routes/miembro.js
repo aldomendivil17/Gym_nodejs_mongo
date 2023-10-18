2eaa8209ac664604ebf040c8ef7eef17ef9ab3bd
aldomendivil17 <aldomendivil17@gmail.com>
2023-10-18 14:56:43 -0700

    se agregó miembro routes

---
+++ routes/miembro.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const miembroControl = require('../controllers/miembroControl');
+
+// Rutas
+router.get('/membresia', membresiaControl.getAllMembresias);
+router.post('/membresia', membresiaControl.addMembresia);
+router.get('/membresia/:id', membresiaControl.getMembresiaById);
+router.put('/membresia/:id', membresiaControl.updateMembresia);
+router.delete('/membresia/:id', membresiaControl.deleteMembresia);
+
+module.exports = router;
