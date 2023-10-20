a5f3137b245f2bf810196283503a6f27867c9fed
aldomendivil17 <aldomendivil17@gmail.com>
2023-10-19 17:34:28 -0700

    se modificó la clase route de miembro

--- routes/miembro.js
+++ routes/miembro.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const miembroControl = require('../controllers/miembroControl');
 
 // Rutas
-router.get('/membresia', membresiaControl.getAllMembresias);
-router.post('/membresia', membresiaControl.addMembresia);
-router.get('/membresia/:id', membresiaControl.getMembresiaById);
-router.put('/membresia/:id', membresiaControl.updateMembresia);
-router.delete('/membresia/:id', membresiaControl.deleteMembresia);
+router.get('/miembro', miembroControl.getAllMiembros);
+router.post('/miembro', miembroControl.addMiembro);
+router.get('/miembro/:id', miembroControl.getMiembroById);
+router.put('/miembro/:id', miembroControl.updateMiembro);
+router.delete('/miembro/:id', miembroControl.deleteMiembro);
 
 module.exports = router;
